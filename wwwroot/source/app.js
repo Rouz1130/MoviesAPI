191c4c1f81dcd554bd4d46cf6052b50ddb9c5372
Rouzbeh Majlessi <rouz@Rouzbehs-iMac.local>
2018-12-17 22:36:36 -0800

    Add movie component client side

---
+++ wwwroot/source/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Movie extends React.Component {
+    render() {
+        return (
+            <div>{this.props.movieName} ({this.props.movieYear}) 
+            </div>
+        );
+    }
+};
+
+export default Movie;
