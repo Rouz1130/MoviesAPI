967d7513b92cc210d9d2612fc9411a6f689d43e9
Rouzbeh Majlessi <rouz@Rouzbehs-iMac.local>
2018-12-17 20:58:10 -0800

    Add DbContext class

---
+++ Services/MoviesDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Entities;
+
+namespace MoviesAPI.Services
+{
+    public class  MoviesDbContext : DbContext 
+    {
+        public DbSet<Movie> Movies {get; set; }
+
+        public MoviesDbContext (
+            DbContextOptions<MoviesDbContext> options)
+            : base(options)
+            {
+                Database.EnsureCreated();
+            }
+    }
+}
