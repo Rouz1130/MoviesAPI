42e5c9255a15cc97821bc83997902326d3c09613
Rouzbeh Majlessi <rouz@Rouzbehs-iMac.local>
2018-12-17 22:00:13 -0800

    Add homecontroller to handl url requests to match /home

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("/home")]
+    public class  HomeController : Controller
+    {
+        
+        public IActionResult Index() 
+        {
+            return View();
+        }
+    }
+}
